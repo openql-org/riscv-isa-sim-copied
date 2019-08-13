d8d0ceabc1480e64085cb07b9257765ed387d14b
Kiyohito Yamazaki <kyamaz@openql.org>
2019-08-14 01:47:19 +0900

    updated test_qisa.c

--- tests/test_qisa.c
+++ tests/test_qisa.c
@@ -3,9 +3,6 @@
 #include <stdlib.h>
 #include "encoding.h"
 
-// "\x82\x80\x00\x00"(0x8082) is `c.jr x1`
-#define c_jr_x1 "\x82\x80\x00\x00"
-
 // qmeas.k   rd rs1 rs2 qimm6 31=1 14..12=0 6..2=0x02 1..0=3  # quantum measurement
 // qtelep.k  rd rs1 rs2 qimm6 31=0 14..12=0 6..2=0x02 1..0=3  # quantum teleportation
 // qtocx.k   rd rs1 rs2 qimm6 31=1 14..12=4 6..2=0x02 1..0=3  # quantum two-qubit operator CX-gate
@@ -17,27 +14,84 @@
 // qoot.k    rd rs1 rs2 qimm6 31=0 14..12=6 6..2=0x02 1..0=3  # quantum one-qubit operator T-gate
 // qooh.k    rd rs1 rs2 qimm6 31=0 14..12=7 6..2=0x02 1..0=3  # quantum one-qubit operator H-gate
 
+#define c_jr_x1 "\x82\x80\x00\x00"   // 0x8082 is `c.jr x1`
+
 // rd is x10
-static char* qmeas  = "\x0b\x05\x00\x80" c_jr_x1;
-static char* qtelep = "\x0b\x05\x00\x00" c_jr_x1;
-static char* qtocx  = "\x0b\x45\x00\x80" c_jr_x1;
-static char* qootdg = "\x0b\x15\x00\x00" c_jr_x1;
-static char* qoosdg = "\x0b\x25\x00\x00" c_jr_x1;
-static char* qooz   = "\x0b\x35\x00\x00" c_jr_x1;
-static char* qoox   = "\x0b\x45\x00\x00" c_jr_x1;
-static char* qoos   = "\x0b\x55\x00\x00" c_jr_x1;
-static char* qoot   = "\x0b\x65\x00\x00" c_jr_x1;
-char* qooh   = "\x0b\x75\x00\x00" c_jr_x1;
+#define qtelep_rd5_q1_q0 "\x0b\x85\x00"
+#define qmeas_rd5_q1_q0  "\x0b\x85\x00"  // should be qimm7
+#define qtocx_rd5_q1_q0  "\x0b\xc5\x00"  // should be qimm7
+#define qootdg_rd5_q1_q0 "\x0b\x15\x00"
+#define qoosdg_rd5_q1_q0 "\x0b\x25\x00"
+#define qooz_rd5_q1_q0   "\x0b\xb5\x00"
+#define qoox_rd5_q1_q0   "\x0b\xc5\x00"
+#define qoos_rd5_q1_q0   "\x0b\xd5\x00"
+#define qoot_rd5_q1_q0   "\x0b\xf5\x00"
+#define qooh_rd5_q1_q0   "\x0b\xf5\x00"
+#define qimm6_0 "\x00"
+#define qimm6_1 "\x02"
+#define qimm6_2 "\x04"
+#define qimm6_3 "\x06"
+#define qimm6_4 "\x08"
+#define qimm6_5 "\x0a"
+#define qimm6_6 "\x0c"
+#define qimm6_7 "\x0e"
+#define qimm6_8 "\x10"
+#define qimm6_9 "\x12"
+#define qimm6_a "\x14"
+#define qimm6_b "\x16"
+#define qimm6_c "\x18"
+#define qimm6_d "\x1a"
+#define qimm6_e "\x1c"
+#define qimm6_f "\x1e"
+#define qimm7_0 "\x80"
+#define qimm7_1 "\x82"
+#define qimm7_2 "\x84"
+#define qimm7_3 "\x86"
+#define qimm7_4 "\x88"
+#define qimm7_5 "\x8a"
+#define qimm7_6 "\x8c"
+#define qimm7_7 "\x8e"
+#define qimm7_8 "\x90"
+#define qimm7_9 "\x92"
+#define qimm7_a "\x94"
+#define qimm7_b "\x96"
+#define qimm7_c "\x98"
+#define qimm7_d "\x9a"
+#define qimm7_e "\x9c"
+#define qimm7_f "\x9e"
 
 #define QOP(op,rd,rs1,rs2,qimm6) ((int (*)())op)()  // TODO: to use parameters
 
 int main(int argc, char **argv) {
-    char* ops[] = { qmeas, qtelep, qtocx, qootdg, qoosdg, qooz, qoox, qoos, qoot, qooh, NULL };
+    char* ops[] = { 
+qtelep_rd5_q1_q0 qimm6_0 \
+qooz_rd5_q1_q0 qimm6_0 \
+qooh_rd5_q1_q0 qimm6_0 \
+qoos_rd5_q1_q0 qimm6_0 \
+qmeas_rd5_q1_q0 qimm7_0 \
+c_jr_x1,
+
+// qooh_rd5_q1_q0 "\x02" c_jr_x1,
+// qooh_rd5_q1_q0 "\x04" c_jr_x1,
+// qooh_rd5_q1_q0 "\x06" c_jr_x1,
+// qooh_rd5_q1_q0 "\x08" c_jr_x1,
+// qooh_rd5_q1_q0 "\x0a" c_jr_x1,
+// qooh_rd5_q1_q0 "\x0c" c_jr_x1,
+// qooh_rd5_q1_q0 "\x0f" c_jr_x1,
+// qooh_rd5_q1_q0 "\x10" c_jr_x1,
+// qooh_rd5_q1_q0 "\x12" c_jr_x1,
+// qooh_rd5_q1_q0 "\x14" c_jr_x1,
+// qooh_rd5_q1_q0 "\x16" c_jr_x1,
+// qooh_rd5_q1_q0 "\x18" c_jr_x1,
+// qooh_rd5_q1_q0 "\x1a" c_jr_x1,
+// qooh_rd5_q1_q0 "\x1c" c_jr_x1,
+// qooh_rd5_q1_q0 "\x1f" c_jr_x1,
+      NULL };
     for( char **p = &ops[0]; *p != NULL; p++){
         char *o = *p;
         uint32_t op = (uint8_t)*o + ((uint8_t)*(o+1) <<8) + ((uint8_t)*(o+2) <<16) + ((uint8_t)*(o+3) <<24);
         int c = QOP(*p, /*rd*/ 10, /*rs1*/ 1, /*rs2*/ 2, /*qimm6*/ 3);
-        printf("%d\n", c);
+        printf("%08x: %d\n",op, c);
     }
     return 0;
 }
