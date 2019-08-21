2bed480293cdd457ca9d75684a6b31b31033b12c
Kiyohito Yamazaki <kyamaz@openql.org>
2019-08-21 22:48:59 +0900

    changed qoosdg_k.h/qootdg_k.h using sdgGate/tdgGate

--- riscv/insns/qoosdg_k.h
+++ riscv/insns/qoosdg_k.h
@@ -1,1 +1,6 @@
+#ifdef QUEST
+sdgGate(*(p->get_qubits()), insn.k_qimm6());
+WRITE_RD(insn.k_qimm6());
+#else
 WRITE_RD(2);
+#endif
--- riscv/insns/qootdg_k.h
+++ riscv/insns/qootdg_k.h
@@ -1,1 +1,6 @@
+#ifdef QUEST
+tdgGate(*(p->get_qubits()), insn.k_qimm6());
+WRITE_RD(insn.k_qimm6());
+#else
 WRITE_RD(1);
+#endif
