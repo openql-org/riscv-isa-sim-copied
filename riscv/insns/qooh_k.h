227ce08ba41429e856e012a09f7e1592fc260edd
hirotakaster <hirotakaster@gmail.com>
2019-09-20 00:11:08 +0900

    output debug print&RD(qmeas) return.

--- riscv/insns/qooh_k.h
+++ riscv/insns/qooh_k.h
@@ -2,7 +2,10 @@
 hadamard(p->get_qubits(), insn.k_qimm6());
 STATE.qbit = insn.k_qimm6();
 int old = STATE.qbit;
-WRITE_RD(sext_xlen(old));
+//WRITE_RD(sext_xlen(old));
+//WRITE_RD(RS1);
+// here is debug output.
+fprintf(stderr, "hadamard : rd: %ld, rs1: %ld, rs2: %ld, qimm6: %ld\n", insn.rd(), insn.rs1(), insn.rs2(), insn.k_qimm6());
 serialize();
 #else
 WRITE_RD(7);
