d6dccd06e48b67224bea3687c814e03ac365bf51
hirotakaster <hirotakaster@gmail.com>
2019-09-28 23:56:23 +0900

    call q func.

---
+++ tests/test_qfunc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    int ret1 = 0;
+
+    // call qooh
+    asm volatile(
+    "qooh.k  qa0,qt1,qzero,1;"
+    );
+
+    // call qmeas
+    asm volatile(
+    "qmeas.k  %0,qt1,qzero,1;"
+    :"=r"(ret1)
+    :
+    );
+    printf("%d\n", ret1);
+
+    return 0;
+}
